1268af81582b601bd6af6f2d1a4ad16a5e691df9
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-19 01:20:49 +0600

    Create 8DONATIONTAKA.c

---
+++ PROGRAMMING EXCERCISE TAHMID RAFI/8DONATIONTAKA.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,e,f,g,h,i,j,total=0;
+    scanf("%d%d%d%d%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f,&g,&h,&i,&j);
+    total= a+b+c+d+e+f+g+h+i+j;
+    printf("%d:\n ",total);
+}

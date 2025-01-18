e6fa2ce718555db7733b808315cc6ef339648ce8
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-19 01:20:51 +0600

    Create 9hexadecimalprint.cpp

---
+++ PROGRAMMING EXCERCISE TAHMID RAFI/9hexadecimalprint.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int input;
+    cin>>input;
+    //cout<<"%x"<<input<<endl;
+    //printf("%x\n",input);
+    cout<<input<<endl;
+    cout<<oct<<uppercase<<input<<endl;
+    cout<<hex<<uppercase<<input<<endl;
+   
+
+}
